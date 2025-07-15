7ad8f56d7fb726f71b8d0cd244ff4678b8a8f495
Andrzej Kiełtyka <a.kieltyka@gmail.com>
2025-07-15 08:13:51 +0200

    chore: Editor state is empty

--- app/routes/_index/components/explorer.client.tsx
+++ app/routes/_index/components/explorer.client.tsx
@@ -1,22 +1,10 @@
-import { ToolbarButton, useOperationsEditorState, useVariablesEditorState } from "@graphiql/react";
 import { GraphiQL } from "graphiql";
 import { JSX } from "react";
 
-import { copyToClipboard } from "../helpers/copyToClipboard";
 import { createFetcher } from "../helpers/createFetcher";
-import { encode } from "../helpers/encode";
+import { ShareExplorerQueryButton } from "./shareExplorerQueryButton.client";
 
 export function Explorer(): JSX.Element {
-  const [operations] = useOperationsEditorState();
-  const [variables] = useVariablesEditorState();
-
-  async function onShareExplorer(): Promise<void> {
-    const shareableURL = new URL("/explorer", window.location.origin);
-    if (operations) shareableURL.searchParams.set("query", encode(operations));
-    if (variables) shareableURL.searchParams.set("variables", encode(variables));
-    await copyToClipboard(shareableURL.toString());
-  }
-
   return <GraphiQL
     dangerouslyAssumeSchemaIsValid
     defaultEditorToolsVisibility="variables"
@@ -30,14 +18,7 @@ export function Explorer(): JSX.Element {
           {prettify}
           {copy}
           {merge}
-          <ToolbarButton
-            label="Share your Explorer query"
-            onClick={() => void onShareExplorer()}
-            style={{ textAlign: "center" }}
-            title="Share your Explorer query"
-          >
-            S
-          </ToolbarButton>
+          <ShareExplorerQueryButton/>
         </>
       )}
     </GraphiQL.Toolbar>
---
+++ app/routes/_index/components/shareExplorerQueryButton.client.tsx
@@ -0,0 +1,29 @@
+import { ToolbarButton, useOperationsEditorState, useVariablesEditorState } from "@graphiql/react";
+import { JSX } from "react";
+
+import { copyToClipboard } from "../helpers/copyToClipboard";
+import { encode } from "../helpers/encode";
+
+export function ShareExplorerQueryButton(): JSX.Element {
+  const [operations] = useOperationsEditorState();
+  const [variables] = useVariablesEditorState();
+
+  async function onShareExplorer(): Promise<void> {
+    // RESULT: <empty string> <empty string>
+    console.log(operations, variables)
+
+    const shareableURL = new URL("/explorer", window.location.origin);
+    if (operations) shareableURL.searchParams.set("query", encode(operations));
+    if (variables) shareableURL.searchParams.set("variables", encode(variables));
+    await copyToClipboard(shareableURL.toString());
+  }
+
+  return <ToolbarButton
+    label="Share your Explorer query"
+    onClick={() => void onShareExplorer()}
+    style={{ textAlign: "center" }}
+    title="Share your Explorer query"
+  >
+    S
+  </ToolbarButton>
+}
